05316b0f5585b15c4cf6773d8ad9d2da0ead6899
Jacob Sanford <jsanford@unb.ca>
2014-06-02 19:07:45 -0300

    Utility to quickly generate patch after rebase

---
+++ generatePatchAfterRebase.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+"""Generates a path and interdiff after updating a patch.
+"""
+from gitCmds import GitRepo
+import os
+
+
+git = GitRepo(os.getcwd())
+new_patch_filename = raw_input("Enter Patch Filename #: ")
+git.diff('8.x', output_to_file = new_patch_filename)
